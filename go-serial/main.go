19741361b417dfcfd630e2c9735d29726e4014cb
Markus Wenzel <mail@markus-wenzel.net>
2023-04-05 15:45:02 +0200

    possible solution

---
+++ go-serial/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/tarm/serial"
+)
+
+func scannSerialRX(s *serial.Port) {
+	fmt.Println("got Port")
+
+	scanner := bufio.NewScanner(s)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+}
+
+func main() {
+	c := &serial.Config{Name: "/dev/cu.usbmodem1234561", Baud: 115200}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal("Cannot open port. ", err)
+	}
+
+	go scannSerialRX(s)
+
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Second * 2)
+		_, err = s.Write([]byte("GENJOKE"))
+		time.Sleep(time.Second * 1)
+		fmt.Println("sent")
+		if err != nil {
+			log.Fatal("Cannot write bytes. ", err)
+		}
+	}
+}
---
+++ tusb_serial_device/main/tusb_serial_device_main.c
@@ -0,0 +1,62 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "tinyusb.h"
+#include "tusb_cdc_acm.h"
+#include "sdkconfig.h"
+
+static uint8_t buf[CONFIG_TINYUSB_CDC_RX_BUFSIZE + 1];
+
+char RESP_CMD[]                 = "GENJOKE";
+
+void tinyusb_cdc_rx_callback(int itf, cdcacm_event_t *event)
+{
+    /* initialization */
+    size_t rx_size = 0;
+
+    /* read */
+    esp_err_t ret = tinyusb_cdcacm_read(itf, buf, CONFIG_TINYUSB_CDC_RX_BUFSIZE, &rx_size);
+
+    if (ret != ESP_OK){
+        // error handling here...
+    }
+
+    if (strcmp((const char *)buf, RESP_CMD) == 0)
+    {
+        unsigned char conbuf[32] = {'{','"','j','o','k','e','"',':','"','t','e','s','t','"', '}', '\n'};
+
+        /* write back */
+        tinyusb_cdcacm_write_queue(itf, conbuf, 16);
+        tinyusb_cdcacm_write_flush(itf, 0);
+    }
+}
+
+void app_main(void)
+{
+    const tinyusb_config_t tusb_cfg = {
+        .device_descriptor = NULL,
+        .string_descriptor = NULL,
+        .external_phy = false,
+        .configuration_descriptor = NULL,
+    };
+
+    ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+
+    tinyusb_config_cdcacm_t acm_cfg = {
+        .usb_dev = TINYUSB_USBDEV_0,
+        .cdc_port = TINYUSB_CDC_ACM_0,
+        .rx_unread_buf_sz = 64,
+        .callback_rx = &tinyusb_cdc_rx_callback, // the first way to register a callback
+        .callback_rx_wanted_char = NULL,
+        .callback_line_state_changed = NULL,
+        .callback_line_coding_changed = NULL
+    };
+
+    ESP_ERROR_CHECK(tusb_cdc_acm_init(&acm_cfg));
+}
