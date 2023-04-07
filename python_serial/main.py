7e4fbd006df46a937fcb5fe469946c47db925f21
Markus Wenzel <mail@markus-wenzel.net>
2023-04-07 18:41:54 +0200

    working python

--- python_serial/main.py
+++ python_serial/main.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python3
+
 import serial
 	
 ser = serial.Serial()
 
 ser.baudrate = 115200
 
-ser.port = '/dev/cu.usbmodem14201'
+ser.port = '/dev/cu.usbmodem1234561'
 
 ser.open()
-ser.write(bytes('generatefunnyjoke','UTF-8'))
+ser.write(bytes('GENRAND','UTF-8'))
 
 total = 0
- 
-while total < 82:
-    print(ord(ser.read(1)))
+while total < 256/8:
+    print(ser.read(1))
     total=total+1
 ser.close()
